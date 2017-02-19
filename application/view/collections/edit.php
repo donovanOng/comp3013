cb388f0fb93c37adbbcd704372c02b777df82178
Donovan Ong <donovan.ongyl@gmail.com>
2017-02-19 17:15:49 +0000

    Dropdown to update access rights

---
+++ application/view/collections/edit.php
@@ -0,0 +1,33 @@
+<? if ($userID == $collection->userID) { ?>
+  
+  <!-- Only collection's owner can edit rights -->
+  <form action="<?php echo URL; ?>collection/update" method="POST">
+
+    <label>Upload Rights: </label>
+    <select name="uploadRights" id="uploadRights">
+      <? foreach($privacy as $key=>$privacy_type) { ?>
+          <? if ($key == $collection->uploadRights) { ?>
+            <option selected value="<?= $key ?>"><?= $privacy_type ?></option>
+          <? } else { ?>
+            <option value="<?= $key ?>"><?= $privacy_type ?></option>
+          <? } ?>
+      <? } ?>
+    </select>
+
+    <label>View Rights: </label>
+    <select name="viewRights" id="viewRights">
+      <? foreach($privacy as $key=>$privacy_type) { ?>
+          <? if ($key == $collection->viewRights) { ?>
+            <option selected value="<?= $key ?>"><?= $privacy_type ?></option>
+          <? } else { ?>
+            <option value="<?= $key ?>"><?= $privacy_type ?></option>
+          <? } ?>
+      <? } ?>
+    </select>
+
+    <input type="hidden" name="collectionID" value="<?= $collection->collectionID ?>" >
+    <input type="submit" name="update" value="Update" />
+
+  </form>
+
+<? } ?>
