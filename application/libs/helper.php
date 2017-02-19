3bb63030899c69daf77477f8e393e1ee91a8b678
Donovan Ong <donovan.ongyl@gmail.com>
2017-02-19 18:25:56 +0000

    Put in_array_field in helper file

--- application/libs/helper.php
+++ application/libs/helper.php
@@ -6,4 +6,18 @@ function Redirect($url, $statusCode = 303)
    die();
 }
 
+function in_array_field($needle, $needle_field, $haystack, $strict = false) {
+    if ($strict) {
+        foreach ($haystack as $item)
+            if (isset($item->$needle_field) && $item->$needle_field === $needle)
+                return true;
+    }
+    else {
+        foreach ($haystack as $item)
+            if (isset($item->$needle_field) && $item->$needle_field == $needle)
+                return true;
+    }
+    return false;
+}
+
 ?>
