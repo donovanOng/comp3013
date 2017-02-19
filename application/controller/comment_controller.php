215e075f13db2b70a5dfd7531cc87043b16a88c5
Donovan Ong <donovan.ongyl@gmail.com>
2017-02-19 17:56:30 +0000

    Remove authentication check when creating new comment

--- application/controller/comment_controller.php
+++ application/controller/comment_controller.php
@@ -7,30 +7,25 @@ class CommentController
 
   public function create()
   {
-    if (isset($_SESSION['current_user'])) {
-      $current_user = $_SESSION['current_user'];
+    if (isset($_POST['submit'])) {
+      $photoID = $_POST['photoID'];
+      $content = $_POST['content'];
 
-      if (isset($_POST['submit'])) {
-        $photoID = $_POST['photoID'];
-        $content = $_POST['content'];
-
-        // insert into database
-        $model = new Comment();
-        $result = $model->create($current_user->userID,
-                                $photoID,
-                                $content);
-        if ($result) {
-          $_SESSION['message'] = 'Comment added successfully';
-          Redirect(URL . 'photo/' . $photoID);
-        } else {
-          $_SESSION['message'] = 'Fail to add comment';
-          Redirect(URL . 'photo/' . $photoID);
-        }
-
+      // insert into database
+      $model = new Comment();
+      $result = $model->create($current_user->userID,
+                              $photoID,
+                              $content);
+      if ($result) {
+        $_SESSION['message'] = 'Comment added successfully';
+        Redirect(URL . 'photo/' . $photoID);
+      } else {
+        $_SESSION['message'] = 'Fail to add comment';
+        Redirect(URL . 'photo/' . $photoID);
       }
 
     } else {
-      $_SESSION['message'] = 'Not logged in!';
+      $_SESSION['message'] = 'Missing required POST header';
       Redirect(URL);
     }
   }
