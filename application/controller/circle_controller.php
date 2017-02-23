48be37219e8f06c6ba7c7ef3aaa8443178a33214
Laman <qafqaz9393@mail.ru>
2017-02-23 14:33:31 +0000

    Revert "View circle's messages"
    This reverts commit 4ce002753c3d9a9a9ebba133ea5abcaa7115fd5c.

--- application/controller/circle_controller.php
+++ application/controller/circle_controller.php
@@ -22,24 +22,57 @@ class CircleController
   public function user_index($circle_userID)
   {
     $model = new Circle();
-    $circles = $model->find_user_circle($circle_userID);
+    $circlesAdmin = $model->find_user_circle_admin($circle_userID);
+    $circlesMember = $model->find_user_circle_member($circle_userID);
 
     require APP . 'view/_templates/header.php';
     require APP . 'view/circles/index.php';
     require APP . 'view/_templates/footer.php';
   }
 
-    public function view($circleID)
+  public function new()
   {
-    // display photos from single collection
-    $model = new Circle();
-    $message = $model->find_message_by_circleID($circleID);
 
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/circles/view.php';
-    require APP . 'view/_templates/footer.php';
+    if (isset($_GET['name'])) {
 
+      $circle_name = $_GET['name'];
+      $model = new Circle();
+      $result = $model->create($circle_name, 
+                               $this->current_userID);
+
+      if ($result) {
+        $_SESSION['message'] = 'Circle created!';
+      } else {
+        $_SESSION['message'] = 'Fail to create circle!';
+      }
+
+    }
+
+    Redirect(URL . 'circle');
   }
+
+  public function delete()
+  {
+    if (isset($_GET['circleID'])) {
+        $circleID = $_GET['circleID'];
+
+        $model = new Circle();
+        $result = $model->delete($circleID);
+
+        if ($result) {
+          $_SESSION['message'] = 'Circle deleted!';
+        } else {
+          $_SESSION['message'] = 'Fail to delete circle!';
+        }
+
+        Redirect(URL . 'circle');
+
+    } else {
+      $_SESSION['message'] = 'No Circle ID';
+      Redirect(URL . 'circle');
+    }
+  }
+
 }
 
 ?>
--- application/model/circle.php
+++ application/model/circle.php
@@ -4,9 +4,9 @@ require_once APP . 'core/model.php';
 
 class Circle extends Model
 {
-
-  public function find_user_circle($userID)
+  public function find_user_circle_admin($userID)
   {
+    // Circles that User manage
     $sql = "SELECT *
             FROM circle
             WHERE userID = :userID";
@@ -17,18 +17,44 @@ class Circle extends Model
     return $query->fetchAll();
   }
 
-  public function find_message_by_circleID($circleID)
+  public function find_user_circle_member($userID)
   {
-    $sql = "SELECT *
-            FROM message
-            WHERE circleID = :circleID";
+    //Circles that User belong to but do not manage
+    $sql = "SELECT c.* 
+            FROM circle as c, circlefriends as cf 
+            WHERE cf.userID = :userID 
+            AND cf.circleID = c.circleID
+            AND c.circleID NOT IN (
+              SELECT circleID 
+              FROM circle 
+              WHERE userID = :userID 
+            )";
 
-    // TODO: Join user and blog to get detailed information
+    $query = $this->db->prepare($sql);
+    $params = array(':userID' => $userID);
+    $query->execute($params);
+    return $query->fetchAll();
+  }
+
+  public function create($name, $userID)
+  {
+    $sql = "INSERT INTO circle (userID, name)
+            VALUES (:userID, :name)";
+
+    $query = $this->db->prepare($sql);
+    $params = array(':userID' => $userID,
+                    ':name' => $name);
+    return $query->execute($params); // boolean result
+  }
+
+  public function delete($circleID)
+  {
+    $sql = "DELETE FROM circle
+            WHERE circleID = :circleID";
 
     $query = $this->db->prepare($sql);
     $params = array(':circleID' => $circleID);
-    $query->execute($params);
-    return $query->fetchAll();
+    return $query->execute($params); // boolean result
   }
 
 }
