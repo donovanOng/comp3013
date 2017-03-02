46a1bb96b3c4513f8450d3479806ef2e90f68ea6
chevyng <seasoneva@gmail.com>
2017-03-02 18:17:56 +0000

    Fixed Friend's page where logged user is able to see other users' friend requests

--- application/view/friends/index.php
+++ application/view/friends/index.php
@@ -26,52 +26,53 @@
   </div>
 </div>
 <? } ?>
+<? if($this->current_userID == $user->userID) { ?>
+    <div class="card mb-3">
+      <div class="card-block">
+        <h4 class="card-title">List of Friend Requests sent</h4>
+        <h6 class="card-subtitle mb-3 text-muted">Number of friend requests sent: <?= count($friend_req_sent) ?></h6>
+        <? if (count($friend_req_sent) > 0) { ?>
+          <?php foreach ($friend_req_sent as $friend) { ?>
+            <div class="row rounded bg-faded align-items-center mr-0 ml-0 mb-2 pr-2 pl-2 pt-3 pb-3">
+              <div class="col-8">
+                <h4>
+                  <a href="<?= URL; ?><?= $friend->userID ?>"><?= $friend->first_name ?>
+                    <?= $friend->last_name ?>
+                  </a>
+                </h4>
+                <h6 class="text-muted"><?= $friend->email ?></h6>
+              </div>
+              <div class="col-4 text-right">
+                <a class="btn btn-secondary" href="<?= URL ?>user/reject_friendships?userID=<?= $friend->userID ?>">Delete Request</a>
+              </div>
+            </div>
+          <?php } ?>
+        <? } ?>
+      </div>
+    </div>
 
-<div class="card mb-3">
-  <div class="card-block">
-    <h4 class="card-title">List of Friend Requests sent</h4>
-    <h6 class="card-subtitle mb-3 text-muted">Number of friend requests sent: <?= count($friend_req_sent) ?></h6>
-    <? if (count($friend_req_sent) > 0) { ?>
-      <?php foreach ($friend_req_sent as $friend) { ?>
-        <div class="row rounded bg-faded align-items-center mr-0 ml-0 mb-2 pr-2 pl-2 pt-3 pb-3">
-          <div class="col-8">
-            <h4>
-              <a href="<?= URL; ?><?= $friend->userID ?>"><?= $friend->first_name ?>
-                <?= $friend->last_name ?>
-              </a>
-            </h4>
-            <h6 class="text-muted"><?= $friend->email ?></h6>
-          </div>
-          <div class="col-4 text-right">
-            <a class="btn btn-secondary" href="<?= URL ?>user/reject_friendships?userID=<?= $friend->userID ?>">Delete Request</a>
-          </div>
-        </div>
-      <?php } ?>
-    <? } ?>
-  </div>
-</div>
-
-<div class="card mb-3">
-  <div class="card-block">
-    <h4 class="card-title">List of Friend Requests received</h4>
-    <h6 class="card-subtitle mb-3 text-muted">Number of friend requests received: <?= count($friend_req_received) ?></h6>
-    <? if (count($friend_req_received) > 0) { ?>
-      <?php foreach ($friend_req_received as $friend) { ?>
-        <div class="row rounded bg-faded align-items-center mr-0 ml-0 pr-2 pl-2 mb-2 pt-3 pb-3">
-          <div class="col-8">
-            <h4>
-              <a href="<?= URL; ?><?= $friend->userID ?>"><?= $friend->first_name ?>
-                <?= $friend->last_name ?>
-              </a>
-            </h4>
-            <h6 class="text-muted"><?= $friend->email ?></h6>
-          </div>
-          <div class="col-4 text-right">
-            <a class="btn btn-primary" href="<?= URL ?>user/accept_friendships?userID=<?= $friend->userID ?>">Accept</a>
-            <a class="btn btn-secondary" href="<?= URL ?>user/reject_friendships?userID=<?= $friend->userID ?>">Delete Request</a>
-          </div>
-        </div>
-      <?php } ?>
-    <? } ?>
-  </div>
-</div>
+    <div class="card mb-3">
+      <div class="card-block">
+        <h4 class="card-title">List of Friend Requests received</h4>
+        <h6 class="card-subtitle mb-3 text-muted">Number of friend requests received: <?= count($friend_req_received) ?></h6>
+        <? if (count($friend_req_received) > 0) { ?>
+          <?php foreach ($friend_req_received as $friend) { ?>
+            <div class="row rounded bg-faded align-items-center mr-0 ml-0 pr-2 pl-2 mb-2 pt-3 pb-3">
+              <div class="col-8">
+                <h4>
+                  <a href="<?= URL; ?><?= $friend->userID ?>"><?= $friend->first_name ?>
+                    <?= $friend->last_name ?>
+                  </a>
+                </h4>
+                <h6 class="text-muted"><?= $friend->email ?></h6>
+              </div>
+              <div class="col-4 text-right">
+                <a class="btn btn-primary" href="<?= URL ?>user/accept_friendships?userID=<?= $friend->userID ?>">Accept</a>
+                <a class="btn btn-secondary" href="<?= URL ?>user/reject_friendships?userID=<?= $friend->userID ?>">Delete Request</a>
+              </div>
+            </div>
+          <?php } ?>
+        <? } ?>
+      </div>
+    </div>
+<? } ?>
