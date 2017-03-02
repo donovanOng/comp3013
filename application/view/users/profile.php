6195ed1aa7f6cc3e77ded1e02a4967188982e124
chevyng <seasoneva@gmail.com>
2017-03-02 11:48:27 +0000

    View friend's profile (Privacy setting not done)

--- application/view/users/profile.php
+++ application/view/users/profile.php
@@ -30,6 +30,17 @@
 
       <? if($isFriend->status == 0) { ?>
         <h3> We are friends! </h3>
+        <? if($profile != NULL) { ?>
+          <p> About : <?= $profile->about ?></p>
+          <p> Gender : <?= $profile->gender ?></p>
+          <p> Birthdate : <?= $profile->birthdate ?></p>
+          <p> Current city : <?= $profile->current_city ?></p>
+          <p> Home city : <?= $profile->home_city ?></p>
+          <p> Address : <?= $profile->address ?></p>
+          <p> Languages : <?= $profile->languages ?></p>
+          <p> Work place : <?= $profile->workplace ?></p>
+        <? } ?>
+
       <? } else if($isFriend->status == 1) { ?>
             <? if($initiator->userID == $this->current_userID) { ?>
                 <h3>Waiting for <?= $user->first_name . ' ' .  $user->last_name ?> to accept </h3>
