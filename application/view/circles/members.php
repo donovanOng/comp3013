d723b6dff2051e06a3499329c496790b19f6875a
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-02 10:11:47 +0000

    Indicate Owner of Circle and only Owner can remove member

--- application/view/circles/members.php
+++ application/view/circles/members.php
@@ -1,16 +1,16 @@
-<? if (count($members) > 0) { ?>
 <?php foreach ($members as $member) { ?>
   <div class="row pl-2 pr-2 mb-1 mt-1">
    <div class="col-5">
       <a href="<?= URL . $member->userID ?>">User <?= $member->userID ?></a>
    </div>
    <div class="col-7 text-right">
-     <? if ($member->userID != $circle->userID) { ?>
+     <? if ($member->userID == $circle->userID) { ?>
+       Owner
+     <? } else if ($this->current_userID == $circle->userID) { ?>
        <a href="<?= URL . 'circle/remove_member?userID=' . $member->userID . '&circleID=' . $circleID ?>">
          Remove
        </a>
-     <? } ?>
+      <? } ?>   
    </div>
  </div>
 <?php } ?>
-<? } ?>
