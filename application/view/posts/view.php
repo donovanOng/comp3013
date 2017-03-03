1c1f55d961a3da723f45bc1df869ecf14b3f90d2
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-03 00:27:33 +0000

    Style for single post

--- application/view/posts/view.php
+++ application/view/posts/view.php
@@ -1,13 +1,15 @@
-<div class="container">
-  <? if ($post != NULL) { ?>
-    <h3><?= $post->title ?> belong to blogID <?= $post->blogID ?></h3>
-    <p>
-      <?= $post->body ?>
-    </p>
-    <a href="<?= URL . "post/delete?blogID="  . $post->blogID  . '&postID=' . $post->postID ?>">
-      Delete Post
-    </a>
-  <? } else { ?>
-      <p>Post with id = <?= $postID ?> doesn't exist!</p>
-  <? } ?>
+<div class="row justify-content-center pt-4 pb-6">
+  <div class="col-6">
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title"><?= $post->title ?></h4>
+      <h6 class="card-subtitle mb-2 text-muted"><?= $post->CREATED_AT ?></h6>
+      <p class="card-text"><?= $post->body ?></p>
+      <a class="card-link" href="<?= URL . "post/delete?blogID="  . $post->blogID  . '&postID=' . $post->postID ?>">
+        Delete Post
+      </a>
+      <a href="#" class="card-link">Edit</a>
+    </div>
+  </div>
+  </div>
 </div>
