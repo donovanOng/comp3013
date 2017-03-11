1f334fc80cf3f81fed3785031048b6dd207baad1
chevyng <seasoneva@gmail.com>
2017-03-11 17:30:40 +0000

    Add server password

--- application/config/config.php
+++ application/config/config.php
@@ -18,6 +18,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'group8');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
+//define('DB_PASS', 'COMP3013');
 define('DB_CHARSET', 'utf8');
 
 ?>
