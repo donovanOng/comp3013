49149252e84d0dc61a31671e11c5c54ccf54bfb8
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-13 22:43:29 +0000

    Add sudo to shell script

--- deploy/install_lamp.sh
+++ deploy/install_lamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-add-repository -y ppa:ondrej/php
-apt-get -y update
+sudo apt-add-repository -y ppa:ondrej/php
+sudo apt-get -y update
 
 # set up a silent install of MySQL
 dbpass=$1
@@ -10,25 +10,25 @@ echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
-apt-get -y install apache2 mysql-server php7.0 php7.0-mysql
+sudo apt-get -y install apache2 mysql-server php7.0 php7.0-mysql
 
 # write some PHP
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
-apt-get -y install git
+sudo apt-get -y install git
 
-git clone https://github.com/donovanOng/comp3013.git /var/www/comp3013/
+sudo git clone https://github.com/donovanOng/comp3013.git /var/www/comp3013/
 
 # Point to project folder
-cp /var/www/comp3013/deploy/000-default.conf /etc/apache2/sites-enabled/
+sudo cp /var/www/comp3013/deploy/000-default.conf /etc/apache2/sites-enabled/
 
 # Enable URL rewrite
-cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
+sudo cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
 
-mysql -h localhost -u root -p $dbpass < /var/www/comp3013/script/create.sql
-mysql -h localhost -u root -p $dbpass < /var/www/comp3013/script/insert.sql
+sudo ysql -h localhost -u root -p $dbpass < /var/www/comp3013/script/create.sql
+sudo mysql -h localhost -u root -p $dbpass < /var/www/comp3013/script/insert.sql
 
 export MYSQL_PASS=$dbpass
 
 # restart Apache
-apachectl restart
+sudo apachectl restart
