f7810a4c2b4482a4059319e64e334474a67df154
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-13 23:11:07 +0000

    Remove db

--- deploy/install_lamp.sh
+++ deploy/install_lamp.sh
@@ -5,10 +5,6 @@ sudo apt-get -y update
 # set up a silent install of MySQL
 dbpass=$1
 
-export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
 # install the LAMP stack
 sudo apt-get -y install apache2 mysql-server php7.0 php7.0-mysql
 
@@ -24,7 +20,6 @@ sudo cp /var/www/comp3013/deploy/000-default.conf /etc/apache2/sites-enabled/
 
 # Enable URL rewrite
 sudo cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
-
 
 # restart Apache
 sudo apachectl restart
