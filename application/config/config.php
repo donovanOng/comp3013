84ad24066619061cbe408748fd83f7bc820b6ca3
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-13 20:34:25 +0000

    Use environment variable if exist

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,12 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'group8');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+if ( getenv('MYSQL_PASS')) {
+  define('DB_PASS', getenv('MYSQL_PASS'));
+} else {
+  define('DB_PASS', 'root');
+}
+
 //define('DB_PASS', 'COMP3013');
 define('DB_CHARSET', 'utf8');
 
