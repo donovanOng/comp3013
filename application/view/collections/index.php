c3799eefca8af60829c7f7fad5699364feb0a5b7
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-14 11:15:22 +0000

    Stop checking collection access when display collection’s index

--- application/view/collections/index.php
+++ application/view/collections/index.php
@@ -13,25 +13,22 @@
     <div class="mt-3 mb-3" style="border-top: 1px solid #DDD;"></div>
       <?php if ($collections_owned != NULL) { ?>
         <div class="row">
-        <?php $privacy = array('Friends', 'Friends of Friends', 'Public'); ?>
         <?php foreach ($collections_owned as $collection) { ?>
-          <?php if (in_array($collection->collectionID, array_column($collections_access, "collectionID"))) { ?>
             <div class="col-3">
               <div class="card mb-3">
-                <img class="card-img-top" src="<?php echo URL . $collection->coverPhoto ?>" onError="this.src='<?php echo URL ?>images/missing.jpg'" style="object-fit: cover; height: 200px;">
+                <img class="card-img-top" src="<?php echo $collection->coverPhoto ?>" onError="this.src='<?php echo URL ?>images/missing.jpg'" style="object-fit: cover; height: 200px;">
                 <div class="card-block">
                   <h5 class="card-title">Collection <?php echo $collection->collectionID ?></h5>
                   <p class="card-text text-muted">
                     <?php echo $collection->noOfPhotos ?> photos
                   </p>
                   <a href="<?php echo URL; ?>collection/<?php echo $collection->collectionID ?>" class="card-link">View Collection</a>
-                  <?php if ($user->userID == $this->current_userID) { ?>
+                  <?php if ($collection->userID == $this->current_userID) { ?>
                     <a href="<?php echo URL; ?>collection/delete?collectionID=<?php echo $collection->collectionID ?>" class="card-link">Delete</a>
                   <?php } ?>
                 </div>
               </div>
             </div>
-          <?php } ?>
         <?php } ?>
       <?php } ?>
     </div>
