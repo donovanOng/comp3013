b2302f22fdf27ff0cfa68c400ae200a9038106de
Laman <qafqaz9393@mail.ru>
2017-03-14 21:25:33 +0000

    Export/Import

--- import.php
+++ import.php
@@ -1,9 +1,23 @@
 <?php
 ini_set('display_errors', 1);
-$db = new PDO('mysql:host=localhost;dbname=group8', 'user', 'password');
+
+$con2 = mysqli_connect("localhost","root","root");
+if (!$con2)  {  
+    die('Could not connect: ' . mysql_error());  
+}
+
+$selectdb = mysqli_select_db($con2, "group8");
+if (!$selectdb)  { 
+    die('Database not used: ; ' . mysql_error());  
+}
+
+echo "connected to DB<br /><br />";
+
+$db = new PDO('mysql:host=localhost;dbname=group8', 'root', 'root');
 
 $xmldoc=new DOMDocument();
-$xmldoc->load('user_data.xml');
+$xmldoc->load('new_data.xml');
+
 
 $xmldata1 = $xmldoc -> getElementsByTagName('annotation');
 $xmlcount1 = $xmldata1 -> length;
@@ -14,39 +28,54 @@ $photoID = $xmldata1->item($i)->getElementsByTagName('photoID')->item(0)->childN
 $userID = $xmldata1->item($i)->getElementsByTagName('userID')->item(0)->childNodes->item(0)->nodeValue;
 $CREATED_AT = $xmldata1->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata1->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
-// $address = $xmldata->item($i)->getElementsByTagName('address')->item(0)->childNodes->item(0)->nodeValue;
-$stmt = $db->prepare("insert into annotation values(?,?,?,?)");
-$stmt -> bindParam(1,$photoID);
-$stmt -> bindParam(2,$userID);
-$stmt -> bindParam(3,$CREATED_AT);
-$stmt -> bindParam(4,$UPDATED_AT);
-// $stmt -> bindParam(5,$address);
-$stmt -> execute();
-// printf($name.'<br/>');
+
+    $check="SELECT count(*) FROM annotation WHERE photoID = '$photoID' AND userID = '$userID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    if ($data[0] > 0) {
+        $query = "UPDATE annotation SET photoID = '$photoID', userID='$userID', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE photoID = '$photoID' AND userID = '$userID'"; 
+    } else {
+        $query = "INSERT INTO annotation (photoID, userID, CREATED_AT, UPDATED_AT)
+                        VALUES ('$photoID', '$userID', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+
 }
+
 
 $xmldata2 = $xmldoc -> getElementsByTagName('blog');
 $xmlcount2 = $xmldata2 -> length;
 
 for ($i=0; $i<$xmlcount2; $i++) {
 
-$blogID = $xmldata2->item($i)->getElementsByTagName('blogID')->item(0)->childNodes->item(0)->nodeValue;
-$userID = $xmldata2->item($i)->getElementsByTagName('userID')->item(0)->childNodes->item(0)->nodeValue;
-$name = $xmldata2->item($i)->getElementsByTagName('name')->item(0)->childNodes->item(0)->nodeValue;
-$CREATED_AT = $xmldata2->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
-$UPDATED_AT = $xmldata2->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
+    $blogID = $xmldata2->item($i)->getElementsByTagName('blogID')->item(0)->childNodes->item(0)->nodeValue;
+    $userID = $xmldata2->item($i)->getElementsByTagName('userID')->item(0)->childNodes->item(0)->nodeValue;
+    $name = $xmldata2->item($i)->getElementsByTagName('name')->item(0)->childNodes->item(0)->nodeValue;
+    $CREATED_AT = $xmldata2->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
+    $UPDATED_AT = $xmldata2->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into blog values(?,?,?,?,?)");
-$stmt -> bindParam(1,$blogID);
-$stmt -> bindParam(2,$userID);
-$stmt -> bindParam(3,$name);
-$stmt -> bindParam(4,$CREATED_AT);
-$stmt -> bindParam(5,$UPDATED_AT);
 
-$stmt -> execute();
+    $check="SELECT count(*) FROM blog WHERE blogID = '$blogID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    if ($data[0] > 0) {
+        $query = "UPDATE blog SET userID='$userID', name='$name', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE blogID='$blogID'"; 
+    } else {
+        $query = "INSERT INTO blog (blogID, userID, name, CREATED_AT, UPDATED_AT)
+                        VALUES ('$blogID', '$userID', '$name', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+
+    // $query =    "INSERT INTO blog (blogID, userID, name, CREATED_AT, UPDATED_AT)
+    // VALUES ('$blogID', '$userID', '$name', '$CREATED_AT', '$UPDATED_AT')"; 
+
+    // $query = "UPDATE blog SET blogID='$blogID', userID='$userID', name='$name', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE blogID='$blogID'"; 
 	
 }
-
 
 $xmldata3 = $xmldoc -> getElementsByTagName('circle');
 $xmlcount3 = $xmldata3 -> length;
@@ -59,15 +88,20 @@ $name = $xmldata3->item($i)->getElementsByTagName('name')->item(0)->childNodes->
 $CREATED_AT = $xmldata3->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata3->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into circle values(?,?,?,?,?)");
-$stmt -> bindParam(1,$circleID);
-$stmt -> bindParam(2,$userID);
-$stmt -> bindParam(3,$name);
-$stmt -> bindParam(4,$CREATED_AT);
-$stmt -> bindParam(5,$UPDATED_AT);
 
-$stmt -> execute();
-	
+    $check="SELECT count(*) FROM circle WHERE circleID = '$circleID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    if ($data[0] > 0) {
+        $query = "UPDATE circle SET userID='$userID', name='$name', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE circleID='$circleID'"; 
+    } else {
+        $query = "INSERT INTO circle (circleID, userID, name, CREATED_AT, UPDATED_AT)
+                        VALUES ('$circleID', '$userID', '$name', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
 
 
@@ -82,15 +116,19 @@ $userID = $xmldata4->item($i)->getElementsByTagName('userID')->item(0)->childNod
 $CREATED_AT = $xmldata4->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata4->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into circleFriends values(?,?,?,?,?)");
-$stmt -> bindParam(1,$cFriendsID);
-$stmt -> bindParam(2,$circleID);
-$stmt -> bindParam(3,$userID);
-$stmt -> bindParam(4,$CREATED_AT);
-$stmt -> bindParam(5,$UPDATED_AT);
+    $check="SELECT count(*) FROM circleFriends WHERE cFriendsID = '$cFriendsID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE circleFriends SET circleID='$circleID', userID='$userID', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE cFriendsID='$cFriendsID'"; 
+    } else {
+        $query = "INSERT INTO circleFriends (cFriendsID, circleID, userID, CREATED_AT, UPDATED_AT)
+                        VALUES ('$cFriendsID', $circleID', '$userID', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
 
 
@@ -106,16 +144,19 @@ $content = $xmldata5->item($i)->getElementsByTagName('content')->item(0)->childN
 $CREATED_AT = $xmldata5->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata5->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into comment values(?,?,?,?,?,?)");
-$stmt -> bindParam(1,$commentID);
-$stmt -> bindParam(2,$userID);
-$stmt -> bindParam(3,$photoID);
-$stmt -> bindParam(4,$content);
-$stmt -> bindParam(5,$CREATED_AT);
-$stmt -> bindParam(6,$UPDATED_AT);
+    $check="SELECT count(*) FROM comment WHERE commentID = '$commentID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE comment SET userID='$userID', photoID='$photoID', content='$content', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE commentID='$commentID'"; 
+    } else {
+        $query = "INSERT INTO comment (commentID, userID, photoID, content, CREATED_AT, UPDATED_AT)
+                        VALUES ('$commentID', $userID', '$photoID', '$content', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+
 }
 
 $xmldata6 = $xmldoc -> getElementsByTagName('message');
@@ -130,18 +171,20 @@ $content = $xmldata6->item($i)->getElementsByTagName('content')->item(0)->childN
 $CREATED_AT = $xmldata6->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata6->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into message values(?,?,?,?,?,?)");
-$stmt -> bindParam(1,$messageID);
-$stmt -> bindParam(2,$circleID);
-$stmt -> bindParam(3,$userID);
-$stmt -> bindParam(4,$content);
-$stmt -> bindParam(5,$CREATED_AT);
-$stmt -> bindParam(6,$UPDATED_AT);
+    $check="SELECT count(*) FROM message WHERE messageID = '$messageID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE message SET circleID='$circleID', userID='$userID', content='$content', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE messageID='$messageID'"; 
+    } else {
+        $query = "INSERT INTO message (messageID, circleID, userID, content, CREATED_AT, UPDATED_AT)
+                        VALUES ('$messageID', $circleID', '$userID', '$content', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
-
 
 $xmldata7 = $xmldoc -> getElementsByTagName('photo');
 $xmlcount7 = $xmldata7 -> length;
@@ -155,18 +198,20 @@ $path = $xmldata7->item($i)->getElementsByTagName('path')->item(0)->childNodes->
 $CREATED_AT = $xmldata7->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata7->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into photo values(?,?,?,?,?,?)");
-$stmt -> bindParam(1,$photoID);
-$stmt -> bindParam(2,$collectionID);
-$stmt -> bindParam(3,$userID);
-$stmt -> bindParam(4,$path);
-$stmt -> bindParam(5,$CREATED_AT);
-$stmt -> bindParam(6,$UPDATED_AT);
+    $check="SELECT count(*) FROM photo WHERE photoID = '$photoID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE photo SET collectionID='$collectionID', userID='$userID', path='$path', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE photoID='$photoID'"; 
+    } else {
+        $query = "INSERT INTO photo (photoID, collectionID, userID, path, CREATED_AT, UPDATED_AT)
+                        VALUES ('$photoID', $collectionID', '$userID', '$path', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
-
 
 $xmldata8 = $xmldoc -> getElementsByTagName('photoCollection');
 $xmlcount8 = $xmldata8 -> length;
@@ -179,19 +224,48 @@ $accessRights = $xmldata8->item($i)->getElementsByTagName('accessRights')->item(
 $CREATED_AT = $xmldata8->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata8->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into photoCollection values(?,?,?,?,?)");
-$stmt -> bindParam(1,$collectionID);
-$stmt -> bindParam(2,$userID);
-$stmt -> bindParam(3,$accessRights);
-$stmt -> bindParam(4,$CREATED_AT);
-$stmt -> bindParam(5,$UPDATED_AT);
+    $check="SELECT count(*) FROM photoCollection WHERE collectionID = '$collectionID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE photoCollection SET userID='$userID', accessRights='$accessRights', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE collectionID='$collectionID'"; 
+    } else {
+        $query = "INSERT INTO photoCollection (collectionID, userID, accessRights, CREATED_AT, UPDATED_AT)
+                        VALUES ($collectionID', '$userID', '$accessRights', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
 
-//photoCollectionAccessRights ??????????????????????????????
-//post ???????????????????????????
+
+$xmldata12 = $xmldoc -> getElementsByTagName('post');
+$xmlcount12 = $xmldata12 -> length;
+
+for ($i=0; $i<$xmlcount12; $i++) {
+
+$postID = $xmldata12->item($i)->getElementsByTagName('postID')->item(0)->childNodes->item(0)->nodeValue;
+$blogID = $xmldata12->item($i)->getElementsByTagName('blogID')->item(0)->childNodes->item(0)->nodeValue;
+$title = $xmldata12->item($i)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+$body = $xmldata12->item($i)->getElementsByTagName('body')->item(0)->childNodes->item(0)->nodeValue;
+$CREATED_AT = $xmldata12->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
+$UPDATED_AT = $xmldata12->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
+
+    $check="SELECT count(*) FROM post WHERE postID = '$postID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    if ($data[0] > 0) {
+        $query = "UPDATE post SET blogID='$blogID', title='$title', body='$body', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE postID='$postID'"; 
+    } else {
+        $query = "INSERT INTO post (postID, blogID, title, body, CREATED_AT, UPDATED_AT)
+                        VALUES ($postID', '$blogID', '$title', '$body', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
+}
 
 
 $xmldata9 = $xmldoc -> getElementsByTagName('profile');
@@ -211,21 +285,20 @@ $workplace = $xmldata9->item($i)->getElementsByTagName('workplace')->item(0)->ch
 $CREATED_AT = $xmldata9->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata9->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into profile values(?,?,?,?,?,?,?,?,?,?,?)");
-$stmt -> bindParam(1,$userID);
-$stmt -> bindParam(2,$about);
-$stmt -> bindParam(3,$gender);
-$stmt -> bindParam(4,$birthdate);
-$stmt -> bindParam(5,$current_city);
-$stmt -> bindParam(6,$home_city);
-$stmt -> bindParam(7,$address);
-$stmt -> bindParam(8,$languages);
-$stmt -> bindParam(9,$workplace);
-$stmt -> bindParam(10,$CREATED_AT);
-$stmt -> bindParam(11,$UPDATED_AT);
+    $check="SELECT count(*) FROM profile WHERE userID = '$userID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE profile SET about='$about', gender='$gender', birthdate='$birthdate', current_city='$current_city', home_city='$home_city', address='$address', languages='$languages', workplace='$workplace', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE userID='$userID'"; 
+    } else {
+        $query = "INSERT INTO profile (userID, about, gender, birthdate, current_city, home_city, address, languages, workplace, CREATED_AT, UPDATED_AT)
+                        VALUES ($userID, $about', '$gender', '$birthdate', '$current_city', '$home_city', '$address', '$languages', 'workplace', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+
+    
 }
 
 
@@ -241,16 +314,19 @@ $status = $xmldata10->item($i)->getElementsByTagName('status')->item(0)->childNo
 $CREATED_AT = $xmldata10->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata10->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into relationship values(?,?,?,?,?,?)");
-$stmt -> bindParam(1,$relationshipID);
-$stmt -> bindParam(2,$userID);
-$stmt -> bindParam(3,$userID_2);
-$stmt -> bindParam(4,$status);
-$stmt -> bindParam(5,$CREATED_AT);
-$stmt -> bindParam(6,$UPDATED_AT);
+    $check="SELECT count(*) FROM relationship WHERE relationshipID = '$relationshipID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE relationship SET userID='$userID', userID_2='$userID_2', status='$status', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE relationshipID='$relationshipID'"; 
+    } else {
+        $query = "INSERT INTO relationship (relationshipID, userID, userID_2, CREATED_AT, UPDATED_AT)
+                        VALUES ($relationshipID', '$userID', '$userID_2', '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
 
 
@@ -269,20 +345,25 @@ $admin = $xmldata11->item($i)->getElementsByTagName('admin')->item(0)->childNode
 $CREATED_AT = $xmldata11->item($i)->getElementsByTagName('CREATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 $UPDATED_AT = $xmldata11->item($i)->getElementsByTagName('UPDATED_AT')->item(0)->childNodes->item(0)->nodeValue;
 
-$stmt = $db->prepare("insert into user values(?,?,?,?,?,?,?,?,?)");
-$stmt -> bindParam(1,$userID);
-$stmt -> bindParam(2,$first_name);
-$stmt -> bindParam(3,$last_name);
-$stmt -> bindParam(4,$email);
-$stmt -> bindParam(5,$password);
-$stmt -> bindParam(6,$privacy);
-$stmt -> bindParam(7,$admin);
-$stmt -> bindParam(8,$CREATED_AT);
-$stmt -> bindParam(9,$UPDATED_AT);
+    $check="SELECT count(*) FROM user WHERE userID = '$userID'";
+    $rs = mysqli_query($con2,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
 
-$stmt -> execute();
-	
+    if ($data[0] > 0) {
+        $query = "UPDATE user SET first_name='$first_name', last_name='$last_name', email='$email', password='$password', privacy='$privacy', admin='$admin', CREATED_AT='$CREATED_AT', UPDATED_AT='$UPDATED_AT' WHERE userID='$userID'"; 
+    } else {
+        $query = "INSERT INTO user (userID, first_name, last_name, email, password, privacy, admin, CREATED_AT, UPDATED_AT)
+                        VALUES ($userID', '$first_name', '$last_name', '$email', '$password', '$privacy', '$admin' '$CREATED_AT', '$UPDATED_AT')"; 
+    }
+    echo $query;
+    mysqli_query($con2, $query);
+    
 }
 
+
+    //show updated records            
+    printf ("Records inserted: %d\n", mysqli_affected_rows($con2)); 
+
+mysqli_close($con2);
 
 ?>
