804705fd5a39017f0f534bed0621d5c787c9f837
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-14 08:15:02 +0000

    Update install script

--- deploy/install_lamp.sh
+++ deploy/install_lamp.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-sudo apt-add-repository -y ppa:ondrej/php
-sudo apt-get -y update
+apt-add-repository -y ppa:ondrej/php
+apt-get -y update
+
+# set up a silent install of MySQL
+dbpass=$1
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
-sudo apt-get -y install apache2 mysql-server php7.0 php7.0-mysql
+apt-get -y install apache2 mysql-server php7.0 php7.0-mysql
 
 # write some PHP
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
+apt-get -y install git
+
+git clone https://github.com/donovanOng/comp3013.git /var/www/comp3013/
+
+# Point to project folder
+cp /var/www/comp3013/deploy/000-default.conf /etc/apache2/sites-enabled/
+
+# Enable URL rewrite
+cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
+
+mysql -h localhost -u root -password=$dbpass < /var/www/comp3013/script/create.sql
+mysql -h localhost -u root -password=$dbpass < /var/www/comp3013/script/insert.sql
+
+export MYSQL_PASS=$dbpass
+
 # restart Apache
-sudo apachectl restart
+apachectl restart
