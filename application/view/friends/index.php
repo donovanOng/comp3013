df47ba655dff64d923b8c3b3e48f16875622898c
Donovan Ong <donovan.ongyl@gmail.com>
2017-03-15 09:22:26 +0000

    Refactor post and friends page

--- application/view/friends/index.php
+++ application/view/friends/index.php
@@ -26,53 +26,7 @@
   </div>
 </div>
 <?php } ?>
-<?php if($this->current_userID == $user->userID) { ?>
-    <div class="card mb-3">
-      <div class="card-block">
-        <h4 class="card-title">List of Friend Requests sent</h4>
-        <h6 class="card-subtitle mb-3 text-muted">Number of friend requests sent: <?php echo count($friend_req_sent) ?></h6>
-        <?php if (count($friend_req_sent) > 0) { ?>
-          <?php foreach ($friend_req_sent as $friend) { ?>
-            <div class="row rounded bg-faded align-items-center mr-0 ml-0 mb-2 pr-2 pl-2 pt-3 pb-3">
-              <div class="col-8">
-                <h4>
-                  <a href="<?php echo URL; ?><?php echo $friend->userID ?>"><?php echo $friend->first_name ?>
-                    <?php echo $friend->last_name ?>
-                  </a>
-                </h4>
-                <h6 class="text-muted"><?php echo $friend->email ?></h6>
-              </div>
-              <div class="col-4 text-right">
-                <a class="btn btn-secondary" href="<?php echo URL ?>user/reject_friendship?userID=<?php echo $friend->userID ?>">Delete Request</a>
-              </div>
-            </div>
-          <?php } ?>
-        <?php } ?>
-      </div>
-    </div>
 
-    <div class="card mb-3">
-      <div class="card-block">
-        <h4 class="card-title">List of Friend Requests received</h4>
-        <h6 class="card-subtitle mb-3 text-muted">Number of friend requests received: <?php echo count($friend_req_received) ?></h6>
-        <?php if (count($friend_req_received) > 0) { ?>
-          <?php foreach ($friend_req_received as $friend) { ?>
-            <div class="row rounded bg-faded align-items-center mr-0 ml-0 pr-2 pl-2 mb-2 pt-3 pb-3">
-              <div class="col-8">
-                <h4>
-                  <a href="<?php echo URL; ?><?php echo $friend->userID ?>"><?php echo $friend->first_name ?>
-                    <?php echo $friend->last_name ?>
-                  </a>
-                </h4>
-                <h6 class="text-muted"><?php echo $friend->email ?></h6>
-              </div>
-              <div class="col-4 text-right">
-                <a class="btn btn-primary" href="<?php echo URL ?>user/accept_friendship?userID=<?php echo $friend->userID ?>">Accept</a>
-                <a class="btn btn-secondary" href="<?php echo URL ?>user/reject_friendship?userID=<?php echo $friend->userID ?>">Delete Request</a>
-              </div>
-            </div>
-          <?php } ?>
-        <?php } ?>
-      </div>
-    </div>
+<?php if($this->current_userID == $user->userID) { ?>
+  <?php require APP . 'view/friends/friend_request.php'; ?>
 <?php } ?>
--- application/view/posts/view.php
+++ application/view/posts/view.php
@@ -1,34 +1,29 @@
 <div class="row justify-content-center pt-4 pb-6">
   <div class="col-6">
-  <div class="card">
-    <div class="card-block">
-
-      <h4 class="card-title mb-0"><?php echo $post->title ?></h4>
-
-      <div class="mb-3">
-        <span class="text-muted small">
-          <a href="<?php echo URL . $post->userID ?>"><?php echo user_name($post->userID) ?></a> /
-          <a href="<?php echo URL . 'blog/' . $post->blogID ?>"><?php echo $post->name ?></a>
-          <strong><span class="align-top">.</span></strong>
-          <?php echo $post->CREATED_AT ?>
-        </span>
-      </div>
-
-      <p class="card-text"><?php echo nl2br($post->body) ?></p>
-
-      <?php if ($post->userID == $this->current_userID) { ?>
-        <div class="bg-faded p-2">
-          <button type="button" class="btn-link mr-2" data-toggle="modal" data-target="#editPost">
-            Edit
-          </button>
-          <?php require APP . 'view/posts/edit_post.php'; ?>
-          <a class="card-link text-danger" href="<?php echo URL . "post/delete?blogID="  . $post->blogID  . '&postID=' . $post->postID ?>">
-            Delete
-          </a>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title mb-0"><?php echo $post->title ?></h4>
+        <div class="mb-3">
+          <span class="text-muted small">
+            <a href="<?php echo URL . $post->userID ?>"><?php echo user_name($post->userID) ?></a> /
+            <a href="<?php echo URL . 'blog/' . $post->blogID ?>"><?php echo $post->name ?></a>
+            <strong><span class="align-top">.</span></strong>
+            <?php echo $post->CREATED_AT ?>
+          </span>
         </div>
-      <?php } ?>
-
+        <p class="card-text"><?php echo nl2br($post->body) ?></p>
+        <?php if ($post->userID == $this->current_userID) { ?>
+          <?php require APP . 'view/posts/modal_edit_post.php'; ?>
+          <div class="bg-faded p-2">
+            <button type="button" class="btn-link mr-2" data-toggle="modal" data-target="#editPost">
+              Edit
+            </button>
+            <a class="card-link text-danger" href="<?php echo URL . "post/delete?blogID="  . $post->blogID  . '&postID=' . $post->postID ?>">
+              Delete
+            </a>
+          </div>
+        <?php } ?>
+      </div>
     </div>
-  </div>
   </div>
 </div>
